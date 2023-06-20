7d16af79e1d51abf17e4bfd687d562e60c94bebf
YQN230613 <YQN230613@qq.com>
2023-06-20 16:17:15 +0800

    第一次本地上传

---
+++ src/HelloWorld.java
@@ -0,0 +1,40 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
